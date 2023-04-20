79a841b7889a0cf3035854358388d3671541b4ce
LambertoNeri <neri.lamberto91@gmail.com>
2023-04-20 15:07:06 +0200

    add FOR to invert words

---
+++ palindroma/js/script.js
@@ -0,0 +1,45 @@
+/*Palindroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma */
+
+/* Consigli del giorno
+1. Scriviamo sempre in italiano i passaggi che vogliamo fare
+2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti */
+
+// chiedere con un prompt all'utente di inserire una parola, inserire la parola in const = parola.
+
+const word = prompt('inserisci una parola');
+
+
+//creare un funzione che inverta word e controlli se "inverted" e "word" sono identiche. IF sono uguali = true, Else = falseù
+
+palindromo(word)
+
+
+
+function palindromo(inverted) {
+
+    const dai = []
+    
+    for ( let i = 0 ; i < word.length; i++) {
+        //console.log(word[word.length - (i + 1)])
+
+        dai.push (`${word[word.length - (i + 1)]}`)
+
+    }
+        
+
+   console.log(dai)
+
+   /* if (inverted == word) {
+        console.log ('è palindromo!!');
+        return true
+    } else {
+        console.log ('non è palindromo');
+        return false
+    }  */
+}
+
+
+
+ 
